df7b84f881d4e845e62ddee95ecd13a6e5cfb873
Aryan Das <95759588+arya-domain@users.noreply.github.com>
2022-12-28 15:10:54 +0530

    comm

--- line16.cpp
+++ line16.cpp
@@ -6,57 +6,47 @@ void merge(int arr[], int l, int m, int r, int size)
     int i = l;
     int j = m + 1;
     int k = l;
-
-    /* create temp array */
     int temp[size];
-
-    while (i <= m && j <= r) {
-        if (arr[i] <= arr[j]) {
+    while (i <= m && j <= r)
+    {
+        if (arr[i] <= arr[j])
+        {
             temp[k] = arr[i];
             i++;
             k++;
         }
-        else {
+        else
+        {
             temp[k] = arr[j];
             j++;
             k++;
         }
     }
-    /* Copy the remaining elements of first half, if there are any */
-    while (i <= m) {
+    while (i <= m)
+    {
         temp[k] = arr[i];
         i++;
         k++;
     }
-
-    /* Copy the remaining elements of second half, if there are any */
-    while (j <= r) {
+    while (j <= r)
+    {
         temp[k] = arr[j];
         j++;
         k++;
     }
-
-    /* Copy the temp array to original array */
-    for (int p = l; p <= r; p++) {
+    for (int p = l; p <= r; p++)
+    {
         arr[p] = temp[p];
     }
 }
 
-/* l is for left index and r is
-right index of the
-sub-array of arr to be sorted */
 void mergeSort(int arr[], int l, int r, int size)
 {
-    if (l < r) {
-        // find midpoint
+    if (l < r)
+    {
         int m = (l + r) / 2;
-
-        /* recurcive mergesort first
-  and second halves */
         mergeSort(arr, l, m, size);
         mergeSort(arr, m + 1, r, size);
-
-        // merge
         merge(arr, l, m, r, size);
     }
 }
@@ -69,37 +59,40 @@ int main()
     int myarray[size];
 
     cout << "Enter " << size << " integers in any order: " << endl;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         cin >> myarray[i];
     }
     cout << "Before Sorting" << endl;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         cout << myarray[i] << " ";
     }
     cout << endl;
 
-    mergeSort(myarray, 0, (size - 1), size); // mergesort(arr,left,right) called
+    mergeSort(myarray, 0, (size - 1), size);
 
     cout << "After Sorting" << endl;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         cout << myarray[i] << " ";
-    } 
+    }
     cout << endl;
 
     int m = 0;
     cout << "Enter the number of students \n";
     cin >> m;
 
-    int min = myarray[-1]; 
-    int curr ;
+    int min = myarray[-1];
+    int curr;
     int i = 0;
-    int j = i+(m-1);
-    while (j < (size-1))
-    {   
-        curr = myarray[j]- myarray[i] ;
+    int j = i + (m - 1);
+    while (j < (size - 1))
+    {
+        curr = myarray[j] - myarray[i];
         if (curr < min)
         {
-            min = curr ;
+            min = curr;
         }
         i++;
         j++;
