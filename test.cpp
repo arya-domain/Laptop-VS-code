29b60b0e7f098b12b76cad3ef061fb9f9860482f
Aryan Das <95759588+arya-domain@users.noreply.github.com>
2023-04-29 10:32:38 +0530

    .

--- test.cpp
+++ test.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int main()
 {
     int a = 90;
-    int b = 5 ;
-    // int c = ;
-    // K<<N = K*2^N
-    cout << (1<<b) << endl;
-    // n>>k = n/(2k)
-    cout << (b>>1);
+    int num = 7 ;
 
+    for(int i= 0 ; i<=num; i++)
+    {bitset<3> a(i);
+    cout<<a << endl;}
     return 0;
 }
